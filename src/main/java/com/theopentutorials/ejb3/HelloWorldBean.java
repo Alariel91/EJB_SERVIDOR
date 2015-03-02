166b840aec3c19dcad1eeb38178d9103e4eea5ff
alariel91 <monysaenzcarrero@gmail.com>
2015-03-02 13:18:16 +0100

    servidor EJB

---
+++ src/main/java/com/theopentutorials/ejb3/HelloWorldBean.java
@@ -0,0 +1,20 @@
+package com.theopentutorials.ejb3;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloWorldBean
+ */
+@Stateless
+public class HelloWorldBean implements HelloWorldBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorldBean() {	    
+    }
+    public String sayHello() {
+	    return "Hello World !!!";
+	    }
+
+}
